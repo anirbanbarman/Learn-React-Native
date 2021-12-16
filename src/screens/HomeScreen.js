f8f7ddd6f60b3fa11b6631c003d80fea9e46b7b0
anirbanbarman <anirbandali247@gmail.com>
2021-12-16 22:32:27 +0530

    navigation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,21 @@
  
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TouchableOpacity } from 'react-native';
 
-export default function HomeScreen() {
+export default function HomeScreen({navigation }) {
+
   return (
     <View style={styles.container}>
       <Text style={styles.text} >Hello React</Text>
+      <TouchableOpacity onPress={()=> navigation.navigate('Component', { name: 'Jane' })}>
+
+         <Text>Go to component </Text> 
+      </TouchableOpacity>
+      <TouchableOpacity onPress={()=>navigation.navigate('List', { name: 'Jane' })}>
+
+         <Text>Go to List List</Text> 
+      </TouchableOpacity>
+     
      
     </View>
   );
